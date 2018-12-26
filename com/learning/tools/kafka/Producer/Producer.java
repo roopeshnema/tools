7533292ee259cf7aa16dbb953f45f3f270468ee0
Roopesh <roopesh.nema@gmail.com>
2018-12-26 12:11:25 +0530

    Adding Kafka constant and Producer.

---
+++ com/learning/tools/kafka/Producer/Producer.java
@@ -0,0 +1,16 @@
+
+public class Producer {
+	
+	private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                                            BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                                        LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                                    StringSerializer.class.getName());
+        return new KafkaProducer<>(props)
+    }
+
+}
--- com/learning/tools/kafka/constants/IKafkaConstants.java
+++ com/learning/tools/kafka/constants/IKafkaConstants.java
@@ -4,7 +4,7 @@ public interface IKafkaConstants {
 	public static Integer MESSAGE_COUNT=1000;
 	public static String CLIENT_ID="client1";
 	public static String TOPIC_NAME="demo";
-	public static String GROUP_ID_CONFIG="consumerGroup1";
+	public static String GROUP_ID_CONFIG=" ";
 	public static Integer MAX_NO_MESSAGE_FOUND_COUNT=100;
 	public static String OFFSET_RESET_LATEST="latest";
 	public static String OFFSET_RESET_EARLIER="earliest";
